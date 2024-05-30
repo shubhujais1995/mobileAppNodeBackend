77280a3ba623a5e4e1d0b073000efb123a4a9aae
shubhujais1995 <shubhujais1995@gmail.com>
2024-05-30 09:47:50 +0530

    used string in connection

--- connection/dbConnection.js
+++ connection/dbConnection.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connectDb = async () => {
     try {
 
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect('mongodb+srv://admin:admin@sj-nodeexpresscluster.t7y7moz.mongodb.net/SJ-otp-db?retryWrites=true&w=majority');
         console.log("Database connected", connect.connection.host, connect.connection.name);
 
     } catch (err) {
