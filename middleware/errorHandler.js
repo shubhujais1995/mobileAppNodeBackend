de53fbbbdea80e1f39e6bb46973632fb1750aa8b
shubhujais1995 <shubhujais1995@gmail.com>
2024-12-07 15:48:14 +0530

    checking

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,6 @@
 const { constants } = require("../constant");
 
 const errorHandler = (err, req, res, next) => {
-    
-  
   const statusCode = res.statusCode ? res.statusCode : 500;
   console.log("error has occured -", res.statusCode);
   switch (statusCode) {
