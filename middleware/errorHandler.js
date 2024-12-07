c9adb9ef2e173e141a6c8ae06bd3258357bc24e0
shubhujais1995 <shubhujais1995@gmail.com>
2024-12-07 15:32:21 +0530

    Added a Procfile.

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,6 +2,7 @@ const { constants } = require("../constant");
 
 const errorHandler = (err, req, res, next) => {
     
+  
   const statusCode = res.statusCode ? res.statusCode : 500;
   console.log("error has occured -", res.statusCode);
   switch (statusCode) {
